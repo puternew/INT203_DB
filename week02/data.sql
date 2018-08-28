f3e40f0509aaa23970863ac80b1944eda2d781de
nev <panupong.int@mail.kmutt.ac.th>
2018-08-28 08:59:01 +0700

    create project

---
+++ week02/data.sql
@@ -0,0 +1,19 @@
+Insert into STUDENTS (STUDENT_ID,PREFIX,FIRST_NAME,LAST_NAME,DOB,ADDRESS,DISTRICT,AMPHUR,PROVINCE,ZIPCODE,EMAIL,MOBILE)
+    values (59130505001,null,'Bob','Steve',null,null,null,'Pai','Mae Hong Son',null,
+    'bob@sit.kmutt.ac.th','0818089002');
+
+Insert into STUDENTS (STUDENT_ID,PREFIX,FIRST_NAME,LAST_NAME,DOB,ADDRESS,DISTRICT,AMPHUR,PROVINCE,ZIPCODE,EMAIL,MOBILE)
+    values (59130505002,'Mr.','Mary','Ann',to_date('11-01-99','DD-MM-YY'),'SIT',null,'Muang','Bangkok','10140',
+    'mary@sit.kmutt.ac.th','0818000900');
+
+Insert into STUDENTS (STUDENT_ID,PREFIX,FIRST_NAME,LAST_NAME,DOB,ADDRESS,DISTRICT,AMPHUR,PROVINCE,ZIPCODE,EMAIL,MOBILE)
+    values (59130505003,'Miss','Lilly','Jason',to_date('10-08-90','DD-MM-YY'),null,'Tungkru','Bangmod','Bangkok','10140',
+    'lilly@gmail.com','0890781090');
+    
+Insert into STUDENTS (STUDENT_ID,PREFIX,FIRST_NAME,LAST_NAME,DOB,ADDRESS,DISTRICT,AMPHUR,PROVINCE,ZIPCODE,EMAIL,MOBILE)
+    values (59130505004,'Mr.','John','Cater',to_date('25-06-01','DD-MM-YY'),null,null,'Muang','Samutsakorn','10150',
+    null,null);
+    
+Insert into STUDENTS (STUDENT_ID,PREFIX,FIRST_NAME,LAST_NAME,DOB,ADDRESS,DISTRICT,AMPHUR,PROVINCE,ZIPCODE,EMAIL,MOBILE)
+    values (59130505065,null,'John','Cove',null,null,null,'Pai','Samutprakarn',
+    null,null,'0890989560');
